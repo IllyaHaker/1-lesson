29cbfd53a3cb629c5d0db00283657e552d827b8d
illya <iz3712547@gmail.com>
2024-10-23 16:39:39 +0300

    21.10.2024

--- 1 пара.py
+++ 1 пара.py
@@ -1,14 +1,18 @@
 x = int(input("перше число:"))
 y = int(input("друге число:"))
-c = (input("дія:"))
- if c == ("/"):
-    ans = x / y
- if c == ("/") and y == 0:
+c = input("дія:")
+ans = 0
+
+if c == "/" and y == 0:
     print("на 0 ділити не можна")
- if c == ("+") and y == 0:
+
+if c == "/":
+    ans = x / y
+
+if c == "+":
     ans = x+y
- if c == ("-") and y == 0:
+if c == "-" and y == 0:
     ans = x-y
-if c == ("*") and y == 0:
+if c == "*" and y == 0:
     ans = x * y
 print(ans)
