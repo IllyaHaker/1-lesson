0c06903643d8683203f2aab1da796ba910ff9162
illya <iz3712547@gmail.com>
2024-11-18 17:48:02 +0200

    18.11.2024

---
+++ DB.py
@@ -0,0 +1,24 @@
+import sqlite3
+
+connection = sqlite3.connect("db.sl3", 5)
+cur = connection.cursor()
+
+#=====CREATE=====
+#cur.execute("CREATE TABLE users (login TEXT)")
+#connection.commit()
+
+
+#name = input("Login : ")
+#cur.execute(f"INSERT INTO users (login) VALUES ('{name}');")
+#connection.commit()
+#("Login added")
+#connection.close()
+
+#========SELECT=========
+#cur.execute(f"SELECT * FROM users")
+cur.execute(f"SELECT * FROM users WHERE login= 'Kyril'")
+connection.commit()
+res = cur.fetchall()
+print(res)
+
+connection.close()
