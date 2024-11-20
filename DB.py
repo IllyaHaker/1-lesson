af3048796587b670c77753142efb095e6e8e70ab
illya <iz3712547@gmail.com>
2024-11-20 17:42:14 +0200

    20.11.2024

--- DB.py
+++ DB.py
@@ -16,9 +16,24 @@ cur = connection.cursor()
 
 #========SELECT==========
 #cur.execute(f"SELECT * FROM users")
-cur.execute(f"SELECT * FROM users WHERE login= 'Kyril'")
-connection.commit()
-res = cur.fetchall()
-print(res)
+#cur.execute(f"SELECT * FROM users WHERE login= 'Kyril'")
+#connection.commit()
+#res = cur.fetchall()
+#print(res)
+
+#========UPDATE======
+# name_s = input("OLD Login : ")
+# name_n = input("New Login : ")
+# cur.execute(f"SELECT rowid FROM users WHERE login= '{name_s}';")
+# connection.commit()
+# res = cur.fetchall()
+# if res !=[]:
+#     id = int(res[0][0])
+#     cur.execute(f"UPDATE users SET login='{name_n}' WHERE rowid={id} ")
+#     connection.commit()
+
+#======DELETE======
+# cur.execute(f"DELETE FROM users WHERE rowid=3;")
+# connection.commit()
 
 connection.close()
--- DB2(AnimalKingdom).py
+++ DB2(AnimalKingdom).py
@@ -11,11 +11,38 @@ cur = connection.cursor()
 
 #====add====
 
-name = input("Name : ")
-type = input("Type : ")
-cur.execute(f"INSERT INTO Animals (Name, Type) VALUES ('{name, type}');")
+# name = input("Name : ")
+# type = input("Type : ")
+# cur.execute(f"INSERT INTO Animals (Name, Type) VALUES ('{name}', '{type}');")
+# connection.commit()
+# print("Animal added")
+
+#========UPDATE======
+# name_s = input("OLD Name : ")
+# name_n = input("New Name : ")
+# cur.execute(f"SELECT rowid FROM Animals WHERE Name= '{name_s}';")
+# connection.commit()
+# res = cur.fetchall()
+# if res !=[]:
+#     id = int(res[0][0])
+#     cur.execute(f"UPDATE Animals SET Name='{name_n}' WHERE rowid={id} ")
+#     connection.commit()
+
+#========SELECT======
+
+cur.execute(f"SELECT * FROM Animals WHERE Type= 'Ссавець';")
 connection.commit()
-print("Animal added")
+res = cur.fetchall()
+for row in res:
+    print(f"{row[0]} - {row[1]}")
+
+
+cur.execute(f"SELECT * FROM Animals")
+connection.commit()
+res = cur.fetchall()
+for row in res:
+    print(f"{row[0]} - {row[1]}")
+
 
 
 
