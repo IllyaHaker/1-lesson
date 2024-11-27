0c87f3577f9d0975fadd3cd18d82efe24aa89d4b
illya <iz3712547@gmail.com>
2024-11-27 17:29:16 +0200

    27.11.2024fd

--- we're all going to die.py
+++ we're all going to die.py
@@ -13,48 +13,40 @@ cur = connection.cursor()
 Choice = input("login or register : ")
 if Choice == 'login':
     login = input("login : ")
-    password = input("password : ")
     cur.execute(f"SELECT rowid FROM Users WHERE login= '{login}';")
     connection.commit()
     res = cur.fetchall()
     if res != []:
-        pass
+        password = input("password : ")
+        cur.execute(f"SELECT rowid FROM Users WHERE password= '{password}';")
+        connection.commit()
+        res = cur.fetchall()
+        if res != []:
+            print('Successful login')
+        else:
+            print('incorrect password')
     else:
         print('login not registered')
-    cur.execute(f"SELECT rowid FROM Users WHERE password= '{password}';")
+
+
+if Choice == 'register':
+    login = input("login : ")
+    cur.execute(f"SELECT rowid FROM Users WHERE login= '{login}';")
     connection.commit()
     res = cur.fetchall()
     if res != []:
-        print('Successful login')
-    else:
-        print('incorrect password')
+        print('this login already exists')
+    if res == []:
+        password = input("password : ")
+        cur.execute(f"INSERT INTO Users (login, password) VALUES ('{login}', '{password}');")
+        connection.commit()
+        print("User added")
 
 
-#========UPDATE======
-# name_s = input("OLD Name : ")
-# name_n = input("New Name : ")
-# cur.execute(f"SELECT rowid FROM Animals WHERE Name= '{name_s}';")
-# connection.commit()
-# res = cur.fetchall()
-# if res !=[]:
-#     id = int(res[0][0])
-#     cur.execute(f"UPDATE Animals SET Name='{name_n}' WHERE rowid={id} ")
-#     connection.commit()
-
-#========SELECT======
-
-#cur.execute(f"SELECT * FROM Animals WHERE Type= 'Ссавець';")
-#connection.commit()
-#res = cur.fetchall()
-#for row in res:
-    #print(f"{row[0]} - {row[1]}")
 
 
-#cur.execute(f"SELECT * FROM Animals")
-#connection.commit()
-#res = cur.fetchall()
-#for row in res:
-    #print(f"{row[0]} - {row[1]}")
+
+
 
 
 
